eba324166b3d96ade04c03637d1765f66a01c34f
lg2046 <lg2046@gmail.com>
2009-05-20 13:51:49 +0800

    freeze rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ deploy_dir = "projects/ultratrace_3_0"
 #vlad deploy
 namespace :vlad do
 
-  remote_task :deploy => [:push_code, :update_code, :sql_migrate, :restart_lighttpd, :restart_fcgi, :refresh_crontab]
+  remote_task :deploy => [:push_code, :update_code, :restart_nginx, :restart_mongrel_cluster, :refresh_crontab]
 
   task :push_code do
     notice "---start push code---"
@@ -17,19 +17,19 @@ namespace :vlad do
   remote_task :update_code do
     notice "---start update code---"
     run "cd #{deploy_dir}; /usr/local/git/bin/git pull"
+  end  # 
+    # 
+    # remote_task :sql_migrate do
+    #   notice "---start migrate---"
+    #   run "cd #{deploy_dir}; /usr/bin/rake db:migrate RAILS_ENV=production"
+    # end
+
+  remote_task :restart_nginx do 
+    notice "---start restart nginx---"
+    run "sudo /etc/init.d/nginx restart"
   end
 
-  remote_task :sql_migrate do
-    notice "---start migrate---"
-    run "cd #{deploy_dir}; /usr/bin/rake db:migrate RAILS_ENV=production"
-  end
-
-  remote_task :restart_lighttpd do 
-    notice "---start restart lighttpd---"
-    run "sudo /etc/init.d/lighttpd restart"
-  end
-
-  remote_task :restart_fcgi do 
+  remote_task :restart_mongrel_cluster do 
     notice "---start restart_fcgi---"
     run "sudo /etc/lighttpd/dispatch.sh restart"
   end  
